638f8c16cefe2f9b615382fcee5f7a7874c53e02
arti <arthur@cherba.org>
2016-10-22 20:19:40 -0400

    Include info about application into compiled binary.
    This adds application name, version and commit hash to the binary.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,39 @@
+/*
+ * Copyright 2016 Arthur D. Cherba
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * @file main.cpp Entry point for the application.
  */
 
+#include <iostream>
+
 #include "util/log.h"
+#include "platform/Platform.h"
 
 int main(int argc, char* argv[]) {
   logging::init();
 
   LOGGER(main);
+
+  std::cout << Platform::getApplicationName()
+            << " - " << Platform::getApplicationDescription();
+  std::cout << " Version " << Platform::getVersionStr()
+            << " Commit " << Platform::getCommitHash()
+            << " " << Platform::getBuildType()
+            << std::endl << std::endl;
 
   LTRC_ << "Trace message.";
   LDBG_ << "Debug message.";
---
+++ src/platform/Platform.h
@@ -0,0 +1,62 @@
+#ifndef SLP_POLY_PLATFORM_H
+#define SLP_POLY_PLATFORM_H
+
+/*
+ * @file Platform.h platform namespace for collection of functions to query
+ * system specific information.
+ *
+ * Copyright 2016 Arthur D. Cherba
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string>
+
+/**
+ * @brief Set of function to query system/compilation variables
+ */
+
+class Platform {
+public:
+  static const char* getApplicationName();
+  static const char* getApplicationDescription();
+  static int getMajorVersion();
+  static int getMinorVersion();
+  static int getBugFixVersion();
+  static std::string getVersionStr();
+
+  /**
+   * @brief return current commit hash.
+   */
+  static const char* getCommitHash(void);
+
+  /**
+   * @return Date Time of last committed revision.
+   */
+  static const char* getCommitDateTime();
+
+
+  static const char* getBugReportUrl();
+  static const char* getSite();
+  static const char* getLicenseInfo();
+
+  static const char* getBuildType();
+  static const char* getCompilerName();
+  static int getCompilerVersion();
+  static const char* getCompilerFlags();
+
+  static const char* getCompileDate();
+  static const char* getCompileTime();
+};
+
+#endif  // SLP_POLY_PLATFORM_H_
